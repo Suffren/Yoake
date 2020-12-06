889caeee9f26809f67694994d89e18722463d873
Suffren <regis.cleis@hotmail.fr>
2020-12-06 19:54:01 +0100

    remove react-router

--- App.js
+++ App.js
@@ -1,27 +1,11 @@
 import React from "react";
 import { StyleSheet, Text, View, Platform } from "react-native";
-import { Router, Route, Link } from "./react-router";
-
-const Home = () => <Text>Home</Text>;
-
-const About = () => <Text>About</Text>;
+import About from "./components/About";
 
 const App = () => (
-  <Router>
     <View style={styles.container}>
-      <View style={styles.nav}>
-        <Link to="/">
-          <Text>Home</Text>
-        </Link>
-        <Link to="/about">
-          <Text>About</Text>
-        </Link>
-      </View>
-
-      <Route exact path="/" component={Home} />
-      <Route path="/about" component={About} />
+      <About></About>
     </View>
-  </Router>
 );
 
 const styles = StyleSheet.create({
